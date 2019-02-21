c47cf8c8f3d5a0f5c42f1ff4a5c749e32acd0a2f
João Mendes <mendeslopes.joao@gmail.com>
2019-02-21 09:49:55 -0300

    Incrementação real no arquivo hello_world.py

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,6 +1,6 @@
 #Arquivo de hello world para o tutorial de GIT
 
 print('Olá, mundo!')
-print('Este código está no meu primeiro repositório GIT!')
+print('Este código está no meu primeiro repositório GIT! E ele já está versionado no GIT')
 print('Se quiser conferir, é só acessar:')
 print('\nhttps://github.com/joaomendesln/Git-tutorial\n')
